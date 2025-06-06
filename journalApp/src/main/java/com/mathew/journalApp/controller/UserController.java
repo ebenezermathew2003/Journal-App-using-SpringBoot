35bf641031197d0f63e879c3a5d47c0fa4748129
Ebenezer Mathew <ebenezermathew0711@gmail.com>
2025-06-06 19:45:55 +0530

    commit

---
+++ journalApp/src/main/java/com/mathew/journalApp/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.mathew.journalApp.controller;
+
+import com.mathew.journalApp.entity.JournalEntry;
+import com.mathew.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/journal")
+public class UserController {
+
+
+}
---
+++ journalApp/src/main/java/com/mathew/journalApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mathew.journalApp.repository;
+
+import com.mathew.journalApp.entity.JournalEntry;
+import com.mathew.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
